f2e88b2e335ce3ce26ac45da9d964d9f9739c370
Shaan Khan K <pip000470@gmail.com>
2026-03-08 11:33:50 +0500

    Update 0_PAIR-1.js

--- Priyansh/commands/0_PAIR-1.js
+++ Priyansh/commands/0_PAIR-1.js
@@ -92,8 +92,8 @@ module.exports.run = async function({ api, event, Users }) {
         };
 
         // Positions adjusted for the new background image
-        drawAvatar(avatar1, 162, 226, 110); 
-        drawAvatar(avatar2, 556, 226, 109); 
+        drawAvatar(avatar1, 162, 226, 111); 
+        drawAvatar(avatar2, 556, 226, 111); 
 
         if (!fs.existsSync(cacheDir)) fs.mkdirSync(cacheDir, { recursive: true });
         fs.writeFileSync(cachePath, canvas.toBuffer());
